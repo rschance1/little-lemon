14c354ab5aca1569319d48065d8146f67636f7c9
Appsgalore33 <richardchance1980@gmail.com>
2023-12-10 20:15:16 -0600

    Updated project to show sections

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,251 +1,363 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { Button, FlatList, Image, SectionList, StyleSheet, Text, TouchableOpacity, View } from "react-native"
-import { useUpdateEffect, getSectionListData } from "../utils/utils";
-import debounce from 'lodash.debounce';
-import { Searchbar } from 'react-native-paper';
-import { Alert } from "react-native";
-import { createTable, getMenuItems, filterByQueryAndCategories, saveMenuItems } from '../utils/database';
+import { useEffect, useState, useCallback, useMemo } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SectionList,
+  Alert,
+  Image,
+  Pressable,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
+import debounce from "lodash.debounce";
+import {
+  createTable,
+  getMenuItems,
+  saveMenuItems,
+  filterByQueryAndCategories,
+} from "../utils/database";
+import Filters from "../components/Filters";
+import { getSectionListData, useUpdateEffect } from "../utils/utils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-const styles = StyleSheet.create({
-    heroView: {
-        padding: 12,
-        backgroundColor: 'rgb(64, 84, 77)',
-    },
-    heroViewContent: {
-        flexDirection: 'row'
-    },
-    heroViewHeading: {
-        flex: 1,
-        marginRight: 12
-    },
-    heroViewImage: {
-        width: 120, height: 120,
-        borderRadius: 20
-    },
-    heroViewHeadingName: {
-        fontSize: 45,
-        fontFamily: 'MarkaziText',
-        color: 'rgb(243, 199, 61)', 
-    },
-    heroViewSubHeading: {
-        fontFamily: 'MarkaziText',
-        color: 'white',
-        fontSize: 30,
-        marginBottom: 12
-    },
-    heroViewAbout: {
-        fontFamily: 'Karla',
-        color: 'white',
-        fontSize: 18
-    },
-    filtersContainer: {
-        marginVertical: 16,
-      },
-    filtersTitle: {
-        marginLeft: 16,
-        fontSize: 16,
-        fontWeight: '700',
-        color: '#282828'
-    },
-    item: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 10,
-        flex: 1
-    },
-    itemInfoContainer: {
-        flex: 1
-    },
-    itemName: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    itemDescription: {
-        color: '#495e56'
-    },
-    itemPrice: {
-        color: '#4a5f58',
-        fontWeight: 'bold'
-    },
-    itemImage: {
-        width: 60,
-        height: 60,
-        marginLeft: 5
-    },
-    searchBar: {
-        margin: 12
-    },
-    categoryItemContainer: {
-        backgroundColor: '#edefee',
-        padding: 10,
-        borderRadius: 16
-    },
-    categoryItemText: {
-        fontWeight: '700',
-        fontSize: 17,
-        color: '#495e57'
-    },
-    categoryContentContainer: {
-        padding: 10
+const API_URL =
+  "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json";
+const sections = ["starters", "mains", "desserts"];
+
+const Item = ({ name, price, description, image }) => (
+  <View style={styles.item}>
+    <View style={styles.itemBody}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Text style={styles.price}>${price}</Text>
+    </View>
+    <Image
+      style={styles.itemImage}
+      source={{
+        uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${image}?raw=true`,
+      }}
+    />
+  </View>
+);
+
+const HomeScreen = ({ navigation }) => {
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    orderStatuses: false,
+    passwordChanges: false,
+    specialOffers: false,
+    newsletter: false,
+    image: "",
+  });
+  const [data, setData] = useState([]);
+  const [searchBarText, setSearchBarText] = useState("");
+  const [query, setQuery] = useState("");
+  const [filterSelections, setFilterSelections] = useState(
+    sections.map(() => false)
+  );
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const json = await response.json();
+      const menu = json.menu.map((item, index) => ({
+        id: index + 1,
+        name: item.name,
+        price: item.price.toString(),
+        description: item.description,
+        image: item.image,
+        category: item.category,
+      }));
+      return menu;
+    } catch (error) {
+      console.error(error);
+    } finally {
     }
-});
-
-const Filters = ({ onChange, selections, sections }) => {
-    return (
-      <View style={styles.filtersContainer}>
-        <Text style={styles.filtersTitle}>ORDER FOR DELIVERY!</Text>
-        <FlatList
-            contentContainerStyle={styles.categoryContentContainer}
-            renderItem={({ item, index }) => {
-                return <TouchableOpacity onPress={() => {
-                    onChange(index);
-                }}>
-                    <View style={styles.categoryItemContainer}>
-                        <Text style={styles.categoryItemText}>{item}</Text>
-                    </View>
-                </TouchableOpacity>
-            }}
-            ItemSeparatorComponent={(<View style={{marginHorizontal: 10}}/>)}
-            data={sections}
-            horizontal={true}
-        />
-      </View>
-    );
   };
 
-const Item = ({ name, price, description, image }) => {
-    
-    return (
-        <View style={styles.item} key={name}>
-            <View style={styles.itemInfoContainer}>
-                <Text style={styles.itemName} numberOfLines={1}>{name}</Text>
-                <Text style={styles.itemDescription} numberOfLines={2}>{description}</Text>
-                <Text style={styles.itemPrice}>$ {price}</Text>
-            </View>
-            <Image 
-                source={{uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${image}?raw=true`}}
-                style={styles.itemImage}
-                />
-        </View>
-    );
-};
-const API_URL =
-  'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json';
+  useEffect(() => {
+    (async () => {
+      let menuItems = [];
+      try {
+        await createTable();
+        menuItems = await getMenuItems();
+        if (!menuItems.length) {
+          menuItems = await fetchData();
+          saveMenuItems(menuItems);
+        }
+        const sectionListData = getSectionListData(menuItems);
+        setData(sectionListData);
+        const getProfile = await AsyncStorage.getItem("profile");
+        setProfile(JSON.parse(getProfile));
+      } catch (e) {
+        Alert.alert(e.message);
+      }
+    })();
+  }, []);
 
-export default HomeScreen = () => {
-    const [data, setData] = useState([]);
-    const [searchBarText, setSearchBarText] = useState('');
-    const [query, setQuery] = useState('');
-    const [sections, setSections] = useState([]);
-    const [filterSelections, setFilterSelections] = useState(
-        sections.map(() => false)
-    );
-    const fetchData = async() => {
-        // 1. Implement this function
-        
-        // Fetch the menu from the API_URL endpoint. You can visit the API_URL in your browser to inspect the data returned
-        // The category field comes as an object with a property called "title". You just need to get the title value and set it under the key "category".
-        // So the server response should be slighly transformed in this function (hint: map function) to flatten out each menu item in the array,
-        let response = await fetch(API_URL);
-        let json = await response.json();
-        return json.menu;
+  useUpdateEffect(() => {
+    (async () => {
+      const activeCategories = sections.filter((s, i) => {
+        if (filterSelections.every(item => item === false)) {
+          return true;
+        }
+        return filterSelections[i];
+      });
+      try {
+        const menuItems = await filterByQueryAndCategories(
+          query,
+          activeCategories
+        );
+        const sectionListData = getSectionListData(menuItems);
+        setData(sectionListData);
+      } catch (e) {
+        Alert.alert(e.message);
+      }
+    })();
+  }, [filterSelections, query]);
+
+  const lookup = useCallback(q => {
+    setQuery(q);
+  }, []);
+
+  const debouncedLookup = useMemo(() => debounce(lookup, 500), [lookup]);
+
+  const handleSearchChange = text => {
+    setSearchBarText(text);
+    debouncedLookup(text);
+  };
+
+  const handleFiltersChange = async index => {
+    const arrayCopy = [...filterSelections];
+    arrayCopy[index] = !filterSelections[index];
+    setFilterSelections(arrayCopy);
+  };
+
+  // FONTS
+  const [fontsLoaded] = useFonts({
+    "Karla-Regular": require("../assets/fonts/Karla-Regular.ttf"),
+    "Karla-Medium": require("../assets/fonts/Karla-Medium.ttf"),
+    "Karla-Bold": require("../assets/fonts/Karla-Bold.ttf"),
+    "Karla-ExtraBold": require("../assets/fonts/Karla-ExtraBold.ttf"),
+    "MarkaziText-Regular": require("../assets/fonts/MarkaziText-Regular.ttf"),
+    "MarkaziText-Medium": require("../assets/fonts/MarkaziText-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
+  }, [fontsLoaded]);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                await createTable();
-                let menuItems = await getMenuItems();
+  if (!fontsLoaded) {
+    return null;
+  }
 
-                // The application only fetches the menu data once from a remote URL
-                // and then stores it into a SQLite database.
-                // After that, every application restart loads the menu from the database
-                if (!menuItems.length) {
-                    menuItems = await fetchData();
-                    await saveMenuItems(menuItems);
-                }
-                // let menuItems = await fetchData();
-                let listCategory = [...new Set(menuItems.map(e => e.category))];
-                setSections(listCategory);
-                setFilterSelections(listCategory.map(() => false));
-                setData(menuItems);
-            } catch (e) {
-            // Handle error
-                Alert.alert(e.message);
-            }
-        })();
-    }, []);
-
-    // useUpdateEffect(() => {
-    //     (async () => {
-    //         const activeCategories = sections.filter((s, i) => {
-    //         // If all filters are deselected, all categories are active
-    //         if (filterSelections.every((item) => item === false)) {
-    //             return true;
-    //         }
-    //         return filterSelections[i];
-    //         });
-    //         try {
-    //         const menuItems = await filterByQueryAndCategories(
-    //             query,
-    //             activeCategories
-    //         );
-    //         const sectionListData = getSectionListData(menuItems);
-    //         setData(sectionListData);
-    //         } catch (e) {
-    //         Alert.alert(e.message);
-    //         }
-    //     })();
-    //     }, [filterSelections, query]);
-
-    const lookup = useCallback((q) => {
-        setQuery(q);
-    }, []);
-
-    const debouncedLookup = useMemo(() => debounce(lookup, 500), [lookup]);
-
-    const handleSearchChange = (text) => {
-        setSearchBarText(text);
-        debouncedLookup(text);
-    };
-    const handleFiltersChange = async (index) => {
-        const arrayCopy = [...filterSelections];
-        arrayCopy[index] = !filterSelections[index];
-        setFilterSelections(arrayCopy);
-      };
-    return <View style={{flex: 1}}>
-        <FlatList 
-            ListHeaderComponent={(<View>
-                <View style={styles.heroView}>
-                    <Text style={styles.heroViewHeadingName}>Little Lemon</Text>
-                    <View style={styles.heroViewContent}>
-                        <View style={styles.heroViewHeading}>
-                            <Text style={styles.heroViewSubHeading}>Chicago</Text>
-                            <Text style={styles.heroViewAbout}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</Text>
-                        </View>
-                        <Image source={require('../assets/Hero_image.png')} style={styles.heroViewImage}/>
-                    </View>
-                </View>
-                <Searchbar
-                    placeholder="Search"
-                    placeholderTextColor="black"
-                    onChangeText={handleSearchChange}
-                    value={searchBarText}
-                    style={styles.searchBar}
-                    iconColor="black"
-                    inputStyle={{ color: 'black' }}
-                    elevation={0}
-                />
-                <Filters
-                    sections={sections}
-                    selections={filterSelections}
-                    onChange={handleFiltersChange}
-                />
-            </View>)}
-            data={data}
-            renderItem={({ item }) => (
-                <Item price={item.price} description={item.description} image={item.image} name={item.name}/>
-            )}
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/littleLemonLogo.png")}
+          accessible={true}
+          accessibilityLabel={"Little Lemon Logo"}
         />
+        <Pressable
+          style={styles.avatar}
+          onPress={() => navigation.navigate("ProfileScreen")}
+        >
+          {profile.image ? (
+            <Image source={{ uri: profile.image }} style={styles.avatarImage} />
+          ) : (
+            <View style={styles.avatarEmpty}>
+              <Text style={styles.avatarEmptyText}>
+                {profile.firstName && Array.from(profile.firstName)[0]}
+                {profile.lastName && Array.from(profile.lastName)[0]}
+              </Text>
+            </View>
+          )}
+        </Pressable>
+      </View>
+      <View style={styles.heroSection}>
+        <Text style={styles.heroHeader}>Little Lemon</Text>
+        <View style={styles.heroBody}>
+          <View style={styles.heroContent}>
+            <Text style={styles.heroHeader2}>Chicago</Text>
+            <Text style={styles.heroText}>
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </Text>
+          </View>
+          <Image
+            style={styles.heroImage}
+            source={require("../assets/Hero_image.png")}
+            accessible={true}
+            accessibilityLabel={"Little Lemon Food"}
+          />
+        </View>
+        <Searchbar
+          placeholder="Search"
+          placeholderTextColor="#333333"
+          onChangeText={handleSearchChange}
+          value={searchBarText}
+          style={styles.searchBar}
+          iconColor="#333333"
+          inputStyle={{ color: "#333333" }}
+          elevation={0}
+        />
+      </View>
+      <Text style={styles.delivery}>ORDER FOR DELIVERY!</Text>
+      <Filters
+        selections={filterSelections}
+        onChange={handleFiltersChange}
+        sections={sections}
+      />
+      <SectionList
+        style={styles.sectionList}
+        sections={data}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Item
+            name={item.name}
+            price={item.price}
+            description={item.description}
+            image={item.image}
+          />
+        )}
+        renderSectionHeader={({ section: { name } }) => (
+          <Text style={styles.itemHeader}>{name}</Text>
+        )}
+      />
     </View>
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Constants.statusBarHeight,
+  },
+  header: {
+    padding: 12,
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#dee3e9",
+  },
+  logo: {
+    height: 50,
+    width: 150,
+    resizeMode: "contain",
+  },
+  sectionList: {
+    paddingHorizontal: 16,
+  },
+  searchBar: {
+    marginTop: 15,
+    backgroundColor: "#e4e4e4",
+    shadowRadius: 0,
+    shadowOpacity: 0,
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#cccccc",
+    paddingVertical: 10,
+  },
+  itemBody: {
+    flex: 1,
+  },
+  itemHeader: {
+    fontSize: 24,
+    paddingVertical: 8,
+    color: "#495e57",
+    backgroundColor: "#fff",
+    fontFamily: "Karla-ExtraBold",
+  },
+  name: {
+    fontSize: 20,
+    color: "#000000",
+    paddingBottom: 5,
+    fontFamily: "Karla-Bold",
+  },
+  description: {
+    color: "#495e57",
+    paddingRight: 5,
+    fontFamily: "Karla-Medium",
+  },
+  price: {
+    fontSize: 20,
+    color: "#EE9972",
+    paddingTop: 5,
+    fontFamily: "Karla-Medium",
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+  },
+  avatar: {
+    flex: 1,
+    position: "absolute",
+    right: 10,
+    top: 10,
+  },
+  avatarImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  avatarEmpty: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#0b9a6a",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heroSection: {
+    backgroundColor: "#495e57",
+    padding: 15,
+  },
+  heroHeader: {
+    color: "#f4ce14",
+    fontSize: 54,
+    fontFamily: "MarkaziText-Medium",
+  },
+  heroHeader2: {
+    color: "#fff",
+    fontSize: 30,
+    fontFamily: "MarkaziText-Medium",
+  },
+  heroText: {
+    color: "#fff",
+    fontFamily: "Karla-Medium",
+    fontSize: 14,
+  },
+  heroBody: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  heroContent: {
+    flex: 1,
+  },
+  heroImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 12,
+  },
+  delivery: {
+    fontSize: 18,
+    padding: 15,
+    fontFamily: "Karla-ExtraBold",
+  },
+});
+
+export default HomeScreen;
--- utils/utils.js
+++ utils/utils.js
@@ -1,67 +1,36 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 
-export const SECTION_LIST_MOCK_DATA = [
-    {
-      title: 'Appetizers',
-      data: [
-        {
-          id: '1',
-          title: 'Pasta',
-          price: '10',
-        },
-        {
-          id: '3',
-          title: 'Pizza',
-          price: '8',
-        },
-      ],
-    },
-    {
-      title: 'Salads',
-      data: [
-        {
-          id: '2',
-          title: 'Caesar',
-          price: '2',
-        },
-        {
-          id: '4',
-          title: 'Greek',
-          price: '3',
-        },
-      ],
-    },
-  ];
-
-/**
- * 3. Implement this function to transform the raw data
- * retrieved by the getMenuItems() function inside the database.js file
- * into the data structure a SectionList component expects as its "sections" prop.
- * @see https://reactnative.dev/docs/sectionlist as a reference
- */
 export function getSectionListData(data) {
-  // SECTION_LIST_MOCK_DATA is an example of the data structure you need to return from this function.
-  // The title of each section should be the category.
-  // The data property should contain an array of menu items. 
-  // Each item has the following properties: "id", "title" and "price"
-  // return SECTION_LIST_MOCK_DATA;
-  
-  let map = new Map();
-  data.forEach(element => {
-    let dt = map.get(element.category);
-    let { id, title, price } = element;
-    if (dt) {
-      dt.push({ id, title, price });
-      map.set(element.category, dt);
+  let restructured = [];
+  data.map(item => {
+    let obj = restructured.find(
+      x =>
+        x.name == item.category.charAt(0).toUpperCase() + item.category.slice(1)
+    );
+    if (obj) {
+      restructured[restructured.indexOf(obj)].data.push({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        description: item.description,
+        image: item.image,
+      });
     } else {
-      map.set(element.category, [{ id, title, price }]);
+      restructured.push({
+        name: item.category.charAt(0).toUpperCase() + item.category.slice(1),
+        data: [
+          {
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            description: item.description,
+            image: item.image,
+          },
+        ],
+      });
     }
   });
-  let arr = Array.from(map.keys()).map((element) => ({
-    title: element,
-    data: map.get(element)
-  }));
-  return arr;
+  return restructured;
 }
 
 export function useUpdateEffect(effect, dependencies = []) {
